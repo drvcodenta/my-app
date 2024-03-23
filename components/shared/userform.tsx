a737f69939d85adfbe9be77b0409537c113a2b1f
Dhruv Trivedi <95343534+drvcodenta@users.noreply.github.com>
2024-03-24 01:48:06 +0530

    Completed the Sidebar

--- components/shared/userform.tsx
+++ components/shared/userform.tsx
@@ -16,29 +16,29 @@ function UserForm() {
     <>
     <div className="relative h-screen">
         <button
-        className={`fixed top-1/2 ${isOpen ? 'left-96' : 'left-0'} transform -translate-y-1/2 p-2 transaction-all ease-in-out duration-500 ${isOpen ? 'left-96' : 'left-0'}`}
+        className={`fixed top-1/2 transform -translate-y-1/2 p-2 transaction-all ease-in-out duration-500 ${isOpen ? 'left-96 max-sm:left-64' : 'left-0'}`}
         onClick={toggleSidebar}>
           {isOpen ? <FontAwesomeIcon icon={faAnglesLeft} /> : <FontAwesomeIcon icon={faAnglesRight} />}
         </button>
     </div>
-    <div className={`h-screen pt-8 pb-4 bg-default flex w-96 transition-all ease-in-out duration-500 ${isOpen? 'translate-x-0' :'-translate-x-full'}`}>
+    <div className={`bg-default w-96 max-sm:w-64 h-screen pt-8 pb-4 flex-shrink-0 transition-all ease-in-out duration-500 ${isOpen? 'translate-x-0' :'-translate-x-full'}`}>
         {isOpen &&(
           <div className="absolute flex flex-col text-xl">
-            <div className="text-white flex items-center w-full">
+            <div className="flex flex-wrap text-white items-center w-full">
               <FontAwesomeIcon icon={faUser} className="mr-4 text-2xl" />
               Hello, User
-              <FontAwesomeIcon icon={faBell} className="ml-48 mr-9 text-2xl" />
+              <FontAwesomeIcon icon={faBell} className="ml-48 mr-9 max-sm:ml-16 text-2xl" />
             </div>
             <div className="my-4"></div>
             <div className="text-white bg-extra-dark w-full pb-2 pt-1">
             <FontAwesomeIcon icon={faMessage} className="mr-3" />
               Discussion Forum
             </div>
-            <div className="text-white flex">
+            <div className="text-white">
             <FontAwesomeIcon icon={faCommentsDollar} className="mr-2 pb-2 pt-2" />
               Market Stories
             </div>
-            <div className="text-white ml-8 pb-2 pt-2">
+            <div className="text-white ml-8 pb-2 pt-2 ">
               Sentiment
             </div>
             <div className="text-white ml-8 pb-2 pt-2">
